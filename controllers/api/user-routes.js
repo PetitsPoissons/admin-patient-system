baf43749b2ffbe7c8796364706f91bd7aa9f373c
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-08-26 15:08:19 -0700

    hide password in user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -17,7 +17,9 @@ router.post('/', (req, res) => {
 /****** READ ******/
 /******************/
 router.get('/', (req, res) => {
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -27,6 +29,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       user_id: req.params.id
     }
