de5e0db4eb5620c51ee0735f7a79c784cc25d64b
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-08-27 02:04:50 -0700

    restore User.js and add initial homeRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { User } = require('../models')
+// const passport = require('passport')
+//     , LocalStrategy = require('passport-local').Strategy;
+// router.post('/login', (req, res) => {
+//     User.findOne({
+//         where: {
+//             username: req.body.username
+//         }
+//     }).then(user => {
+//         if (!user) {
+//             res.status(400).json({
+//                 message: 'no user found'
+//             })
+//         }
+//         res.render('users')
+//         // check if the user's password is valid assign it to a variable
+//         // if the pw is wrong, send a 400 error
+//         // save the session or token
+//         // tell the user they are logged in
+//     })
+// })
+
+router.get("/login", (req, res) => res.render("login"));
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const userRoutes = require('./userRoutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/users', userRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
