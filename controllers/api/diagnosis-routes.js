eb884f83e43632a0418a87fe65025df3497a8073
Sean Cone <seandoesitbetter@gmail.com>
2020-08-28 10:51:28 -0700

    Adding client-routes model, seed Diagnosis data

---
+++ controllers/api/diagnosis-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Diagnosis } = require('../../models');
+
+/******************/
+/***** CREATE *****/
+/******************/
+
+
+
+
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,6 +6,7 @@ const clientRoutes = require('./client-routes.js');
 const procedureRoutes = require('./procedure-routes.js');
 const documentRoutes = require('./document-routes.js');
 const recordRoutes = require('./record-routes.js');
+// const diagnosisRoutes = require('./diagnosis-routes.js');
 
 
 router.use('/users', userRoutes);
@@ -14,6 +15,7 @@ router.use('/clients', clientRoutes);
 router.use('/procedures', procedureRoutes);
 router.use('/documents', documentRoutes);
 router.use('/records', recordRoutes);
+// router.use('/diagnosis', diagnosisRoutes);
 
 
 module.exports = router;
