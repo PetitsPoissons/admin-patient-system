0ef30dca0fbbe31060149144f5a0a6e9b6d95202
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-08-28 09:32:53 -0700

    added many-to-many relationship between User and Client

--- models/index.js
+++ models/index.js
@@ -14,36 +14,56 @@ User.belongsTo(Access, {
   foreignKey: 'access_id'
 });
 
-// create associations between User and Record
-User.hasMany(Record, {
+// create associations between User and Client - this allows 
+// both the User and the Client models to query each other's
+// information in the context of a record
+User.belongsToMany(Client, {
+  through: Record,
   foreignKey: 'user_id'
 });
+Client.belongsToMany(User, {
+  through: Record,
+  foreignKey: 'client_id'
+})
+
+// create associations between User and Record
+User.hasMany(Record, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 Record.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 // create associations between Client and Record
 Client.hasMany(Record, {
-  foreignKey: 'client_id'
+  foreignKey: 'client_id',
+  onDelete: 'CASCADE'
 });
 Record.belongsTo(Client, {
-  foreignKey: 'client_id'
+  foreignKey: 'client_id',
+  onDelete: 'CASCADE'
 });
 
 // create associations between Procedure and Record
 Procedure.hasMany(Record, {
-  foreignKey: 'procedure_id'
+  foreignKey: 'procedure_id',
+  onDelete: 'CASCADE'
 });
 Record.belongsTo(Procedure, {
-  foreignKey: 'procedure_id'
+  foreignKey: 'procedure_id',
+  onDelete: 'CASCADE'
 });
 
 // create associations between Document and Record
 Document.hasMany(Record, {
-  foreignKey: 'document_id'
+  foreignKey: 'document_id',
+  onDelete: 'CASCADE'
 });
 Record.belongsTo(Document, {
-  foreignKey:'document_id'
+  foreignKey:'document_id',
+  onDelete: 'CASCADE'
 });
 
 module.exports = {
