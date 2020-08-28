847ac2cac83a2e205327d8224e89cbdc0909adc8
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-08-27 17:59:22 -0700

    quick templating to render documents

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const userRoutes = require('./feUserRoutes');
+const documentRoutes = require('./feDocumentRoutes');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/users', userRoutes);
+router.use('/documents', documentRoutes);
 router.use('/', homeRoutes);
 
 router.use((req, res) => {
