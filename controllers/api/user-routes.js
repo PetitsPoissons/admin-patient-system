a8dfbe51ff58b28bc24ca7e340e41e1538ef8947
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-08-28 22:11:52 -0700

    logic to destroy a session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,6 +42,15 @@ router.post('/login', (req, res) => {
   });
 });
 
+router.post('/logout', (req, res) => {
+  console.log('inside API/USERS/LOGOUT');
+  if (req.session.loggedIn) {
+    req.session.destroy(() => res.status(204).end());
+  } else {
+    res.status(404).end();
+  }
+});
+
 /******************/
 /****** READ ******/
 /******************/
