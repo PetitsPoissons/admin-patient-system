cd182e404e88791e7e2549af55592f1a392e68af
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-09-02 18:06:54 -0700

    latest changes - just before presentation

--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,7 @@ async function loginFormHandler(event) {
       headers: { 'Content-Type': 'application/json' }
     });
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
